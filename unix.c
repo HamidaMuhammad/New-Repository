fa74ca99a9bcde696a8348a47e054694c76d78ba
HamidaMuhammad <hamidasultan@gmail.com>
2025-01-17 19:47:36 -0500

    Update unix.c

--- unix.c
+++ unix.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
+           struct stat st;
         strncat(cmd, argv[1], BUFSIZE - strlen(cmd) - 1);
     system(cmd);
 }
